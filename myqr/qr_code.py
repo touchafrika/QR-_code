013d24e7b8b6955d1cfcb8341b8fee0723a1d460
Thabo Mayika <mayikathabo@gmail.com>
2020-02-14 14:36:07 +0200

    first commit

---
+++ myqr/qr_code.py
@@ -0,0 +1,12 @@
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1,
+    box_size=15,
+    border=5
+    )
+data = 'http://www.teboholekhalo.co.za'
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill="black", back_color='white')
+img.save('Website QR Code.png')
